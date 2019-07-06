757b2a359f8321f62a9e3eee11d1475013f3c503
colddew <88028842@qq.com>
2019-07-06 12:34:47 +0800

    update observer pattern annotation

--- src/main/java/edu/ustc/sse/cdp/behavior/observer/Observer.java
+++ src/main/java/edu/ustc/sse/cdp/behavior/observer/Observer.java
@@ -6,6 +6,8 @@ package edu.ustc.sse.cdp.behavior.observer;
  * 当目标对象的状态变化时，所有依赖于它的观察者对象都会得到通知并自动更新状态
  * 
  * 拉模型传目标对象用于回调，推模型传需要的信息用于广播
+ *
+ * Observer和Subscriber有相同的语意，Subject用作Observer和Observable之间的桥梁或代理
  */
 public interface Observer {
 	
