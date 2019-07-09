4c26435ffe39224c7150ed64c05f1ebb5f85de65
colddew <88028842@qq.com>
2019-07-09 16:10:27 +0800

    update observer pattern annotation

--- src/main/java/edu/ustc/sse/cdp/behavior/observer/Observer.java
+++ src/main/java/edu/ustc/sse/cdp/behavior/observer/Observer.java
@@ -7,7 +7,7 @@ package edu.ustc.sse.cdp.behavior.observer;
  * 
  * 拉模型传目标对象用于回调，推模型传需要的信息用于广播
  *
- * Observer和Subscriber有相同的语意，Subject用作Observer和Observable之间的桥梁或代理
+ * Observer、Subscriber、Reactor、Watcher有相同的语意，Subject用作Observer和Observable之间的桥梁或代理
  */
 public interface Observer {
 	
